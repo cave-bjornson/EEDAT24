edc8c6b0215e08114378574f725f6be29437c425
bjorn <bjorn@DATOR-15>
2026-03-18 10:23:34 +0100

    Add project files.

---
+++ EEDAT24/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+string name = "Bjorn";
+int y = 2;
+double pi = 3.14;
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine(2 + y);
+Console.WriteLine("Skriv in värdet på pi med 2 decimaler");
+var answer = Console.ReadLine();
+if (double.Parse(answer, CultureInfo.InvariantCulture) == pi)
+{
+    Console.WriteLine("Correct!");
+}
